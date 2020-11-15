7115eb332037eae564bb74fc51d4a935c779411d
John Oliver <john@designstreaks.com.au>
2020-11-16 00:40:15 +1100

    Remove DS header from MIT code.

--- src/ToyRobot.Core/Extensions/ObjectCloneExtensions.cs
+++ src/ToyRobot.Core/Extensions/ObjectCloneExtensions.cs
@@ -1,22 +1,4 @@
-﻿/*
- * DESIGNSTREAKS CONFIDENTIAL
- * __________________
- *
- *  Copyright © DesignStreaks - 2010 - 2020
- *  All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of DesignStreaks and its suppliers, if any.
- * The intellectual and technical concepts contained
- * herein are proprietary to DesignStreaks and its suppliers and may
- * be covered by Australian, U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from DesignStreaks.
- */
-
-// ReSharper disable CheckNamespace
+﻿// ReSharper disable CheckNamespace
 
 namespace System
 {
